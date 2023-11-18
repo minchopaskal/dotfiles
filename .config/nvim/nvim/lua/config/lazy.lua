9c2d6039361587d03cf1a0966e15d8013ffab054
minchopaskal <minchopaskal@gmail.com>
2023-11-18 15:29:06 +0200

    Update nvim files

---
+++ .config/nvim/nvim/lua/config/lazy.lua
@@ -0,0 +1,87 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extras modules here
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+-- =========================================================
+-- Lang map
+-- =========================================================
+local en = [[qwertyuiop[]asdfghjkl;'zxcvbnm,./]]
+local bg = [[,уеишщксдзц;ьяаожгтнвмчюйъэфхпрлб]]
+local en_shift = [[QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?]]
+local bg_shift = [[ыУЕИШЩКСДЗЦ§ѝЯАОЖГТНВМЧЮЙЪЭФХПРЛБ]]
+
+local function escape(str)
+  -- You need to escape these characters to work correctly
+  local escape_chars = [[;,."|\]]
+  return vim.fn.escape(str, escape_chars)
+end
+
+vim.opt.langmap = vim.fn.join({
+  -- | `to` should be first     | `from` should be second
+  escape(bg_shift)
+    .. ";"
+    .. escape(en_shift),
+  escape(bg) .. ";" .. escape(en),
+}, ",")
+-- =========================================================
+-- /LangMap
+-- =========================================================
+
+-- DAP
+require("nvim-dap-projects").search_project_config()
+local dap = require("dap")
+local mason_registry = require("mason-registry")
+local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = codelldb_root .. "adapter/codelldb"
+local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = codelldb_path,
+    args = { "--port", "${port}" },
+  },
+}
---
+++ .config/nvim/nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,4 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependancies = "kevinhwang91/promise-async",
+}
